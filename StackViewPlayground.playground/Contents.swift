835f1fcc8f926371cd71469116367f4cb652431b
Anthony Levings <anthonylevings@As-iMac.local>
2015-07-01 10:55:59 +0100

    UIStackView Challenge

---
+++ StackViewPlayground.playground/Contents.swift
@@ -0,0 +1,3 @@
+
+//: # UIStackView Challenge
+//: The challenge is to have a function that takes a stack view and an array of views, and that through the (1) nesting of additional stack views, (2) alteration of the main stack view's axis, and (3) scaling of images, is able without distortion to make the best use of the available space (i.e. display images at their largest possible size).
